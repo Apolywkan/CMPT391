ee42889e1c339db3072b47792570f6bb26dafa72
JasperNakamura <jasperhibou@yahoo.com>
2023-01-18 20:57:59 -0700

    Create components and UI
    For the Login and Class Registration

---
+++ CMPT 391 Student Registration/Registration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPT_391_Student_Registration
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+            string year = "2023";
+            string term = "Winter";
+            term_label.Text = year + " " + term + " Term";
+
+            this.AcceptButton = class_search_btn;
+        }
+    }
+}
