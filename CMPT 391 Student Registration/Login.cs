1159a2b3f5b23d72fc36442a46debf6286e24151
JasperNakamura <vanj2@mymacewan.ca>
2023-01-25 14:49:46 -0700

    Changed Logins

--- CMPT 391 Student Registration/Login.cs
+++ CMPT 391 Student Registration/Login.cs
@@ -56,21 +56,32 @@ namespace CMPT_391_Student_Registration
             string username = username_input.Text;
             string password = password_input.Text;
 
-            if (username == "admin" && password == "password") //change later
-            {
-                Registration registrationForm = new Registration();
-                registrationForm.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Invalid username or password.");
-            }
-        }
+            string queryString = "SELECT * FROM Logins WHERE username = @username AND password = @password";
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            this.AcceptButton = btn_login;
+            myCommand.CommandText = queryString;
+            myCommand.Parameters.AddWithValue("@username", username);
+            myCommand.Parameters.AddWithValue("@password", password);
+
+            try
+            {
+                myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    Console.WriteLine("username: " + myReader["username"]);
+                    Console.WriteLine("password: " + myReader["password"]);
+                    Registration registrationForm = new Registration();
+                    registrationForm.Show();
+                    this.Hide();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid Username or Password");
+            }
+            finally
+            {
+                myReader.Close();
+            }
         }
     }
 }
