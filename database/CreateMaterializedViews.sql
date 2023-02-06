55efe25aa1a657555d1fd47f1d42c6dc7b8a2c6e
Adam Polywkan <82613399+Apolywkan@users.noreply.github.com>
2023-02-06 10:33:21 -0700

    Added materialized view for view classes join
    Added materialized view for view classes join. Run the first part of create materialized view and then run the second if it doesn't work initially.

--- database/CreateMaterializedViews.sql
+++ database/CreateMaterializedViews.sql
@@ -1,23 +1,7 @@
-Create view vWMathCourses
+Create view vWFullCourses
 With SchemaBinding
 as
-SELECT dbo.Course.CourseID from dbo.Course WHERE dbo.Course.CourseID like 'MATH' + '%'
+SELECT S.s, T.grades, TS.day, TS.start_time, TS.endtime from Takes as T, Section as S, Time_slot as TS where T.sec_id = S.sec_id and S.time_slot_id = TS.time_slot_id
 
-Create Unique Clustered Index UIX_vWMathCourses_CourseID
-on dbo.vWMathCourses(CourseID)
-
-Create view vWBusinessCourses
-With SchemaBinding
-as
-SELECT dbo.Course.CourseID from dbo.Course WHERE dbo.Course.CourseID like 'BUSN' + '%'
-
-Create Unique Clustered Index UIX_vWBusinessCourses_CourseID
-on dbo.vWBusinessCourses(CourseID)
-
-Create view vWComputerCourses
-With SchemaBinding
-as
-SELECT dbo.Course.CourseID from dbo.Course WHERE dbo.Course.CourseID like 'CMPT' + '%'
-
-Create Unique Clustered Index UIX_vWComputerCourses_CourseID
-on dbo.vWComputerCourses(CourseID)
+Create Unique Clustered Index UIX_vWMFullCourses_joined
+on vWFullCourses(sec_id)
